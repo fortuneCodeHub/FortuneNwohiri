c1370f198f5aa3a84c79bad02adf354834897e32
fortuneCodeHub <fortunenwohiri@gmail.com>
2025-07-30 08:35:58 +0100

    New commit

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,8 +1,9 @@
 import { useRef } from "react";
 import Card from "../components/Card";
 import { Globe } from "../components/globe";
+import { Frameworks } from "../components/Frameworks";
 import CopyEmailButton from "../components/CopyEmailButton";
-import { Frameworks } from "../components/FrameWorks";
+
 
 const About = () => {
   const grid2Container = useRef();
