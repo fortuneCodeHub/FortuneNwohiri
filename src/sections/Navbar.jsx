9f43cd1fcfbe07ef4daf068a4e03691f1ed5c5df
fortuneCodeHub <fortunenwohiri@gmail.com>
2025-08-04 17:08:17 +0100

    New commit

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
             href="/"
             className="text-xl font-bold transition-colors text-neutral-400 hover:text-white"
           >
-            FortuneCodeHub
+            Fortune
           </a>
           <button
             onClick={() => setIsOpen(!isOpen)}
